a8ac0d6ed72783921de1f69dcf3b1f8e695bfc91
ObviouslyRob <obviouslyrob@gmail.com>
2017-04-08 20:41:34 +0000

    Adds holy_city controller and establishes routes and pages for poems

---
+++ app/controllers/holy_city_controller.rb
@@ -0,0 +1,68 @@
+class HolyCityController < ApplicationController
+    
+    def cythera
+    end
+    
+    def last_train
+    end
+    
+    def tribute
+    end
+    
+    def shady_grove
+    end
+    
+    def known
+    end
+    
+    def ascetic
+    end
+    
+    def diptych
+    end
+    
+    def hajj
+    end
+    
+    def book_of_sleep
+    end
+    
+    def ode
+    end
+    
+    def sonnet
+    end
+    
+    def reel
+    end
+    
+    def wind_trees
+    end
+    
+    def voiceless
+    end
+    
+    def walk
+    end
+    
+    def hard_rain
+    end
+    
+    def debts
+    end
+    
+    def trespasses
+    end
+    
+    def in_the_cathedral
+    end
+    
+    def selection
+    end
+    
+    def holy
+    end
+    
+    def fenway
+    end
+end
